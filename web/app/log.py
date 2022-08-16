bd8feb75e969ab39c35b2f3a214a52f415d250ae
jobrauer <jobrauer@redhat.com>
2022-08-16 15:31:38 +0200

    Implement a simple web GUI

---
+++ web/app/log.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import sys
+
+# clear the log file on startup
+if os.environ.get("LOG_FILE"):
+    with open(os.environ.get("LOG_FILE"), "w"):
+        pass
+
+
+def get_logger(name: str):
+    log_level = os.environ.get('LOG_LEVEL') or logging.DEBUG
+    log_file = os.environ.get('LOG_FILE')
+
+    log_format = '[%(asctime)s] %(levelname)s\t[%(name)s] %(message)s'
+    logger = logging.getLogger(name)
+
+    logger.setLevel(log_level)
+    console_handler = logging.StreamHandler(stream=sys.stdout)
+    console_handler.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(console_handler)
+    if log_file:
+        file_handler = logging.FileHandler(filename=os.environ['LOG_FILE'], mode='a')
+        file_handler.setFormatter(logging.Formatter(log_format))
+        logger.addHandler(file_handler)
+    return logger
